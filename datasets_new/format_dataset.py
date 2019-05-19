62301383c2ef2d3a207578e56b782a3fa68eb853
Graidaris <vladislavby96@gmail.com>
2019-05-19 12:53:54 +0200

    add func tofloat, merge_datasets update convert

--- datasets_new/format_dataset.py
+++ datasets_new/format_dataset.py
@@ -1,35 +1,36 @@
 
-def isnumeric(s):
-    return all(c in "0123456789.+-" for c in s) and any(c in "0123456789" for c in s)
+def tofloat(s):
+    """In our dataset we can meet a variable like '-.9', this function converts this variable to a float """
+
+    integer, mantissa = s.split('.')
+    if integer is ('-' or '+' or ''):
+        integer += '0'
+    return float(integer + '.' + mantissa)
 
 
 def convert(line):
-    return ','.join(str(float(x)) if isnumeric(x)
-                    else "-" for x in line.strip().split(','))
+    """ Convert all variables from int and specific type to float """
+    
+    return ','.join(str(float(x)) if x.isdigit()
+                 else str(tofloat(x)) for x in line.strip().split(','))
 
 
-with open("new_dataset.data", "w") as new_dataset:
+def merge_datasets(list_names_datasets, file_to_fill):
+    for name_dataset in list_names_datasets:
+        with open(name_dataset, "r") as dataset:
+            for line in dataset:
+                if '?' not in line:
+                    file_to_fill.write(convert(line) + '\n')
 
-    with open("processed.hungarian.data", "r") as dataset:
-        for line in dataset:
-            l = convert(line)
-            if '-' not in l:
-                new_dataset.write(l + '\n')
 
-    with open("processed.switzerland.data", "r") as dataset:
-        for line in dataset:
-            l = convert(line)
-            if '-' not in l:
-                new_dataset.write(l + '\n')
+if __name__ == '__main__':
 
-    with open("processed.va.data", "r") as dataset:
-        for line in dataset:
-            l = convert(line)
-            if '-' not in l:
-                new_dataset.write(l + '\n')
-
-    with open("processed.cleveland.data", "r") as dataset:
-        for line in dataset:
-            l = convert(line)
-            if '-' not in l:
-                new_dataset.write(l + '\n')
+    with open("new_dataset.data", "w") as new_dataset:
+        list_of_datasets_names = [
+            "processed.hungarian.data",
+            "processed.switzerland.data",
+            "processed.va.data",
+            "processed.cleveland.data"
+        ]
+        
+        merge_datasets(list_of_datasets_names, new_dataset)
