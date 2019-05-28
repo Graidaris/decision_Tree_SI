becce6766982eea78ae877467c00cccb7cd9fb36
Graidaris <vladislavby96@gmail.com>
2019-05-28 13:48:57 +0200

    add interview.ui, update interface.py

--- interface.py
+++ interface.py
@@ -1,1 +1,18 @@
-#will be created with helpful QT5 designer
+# File: main.py
+import sys
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QApplication
+from PySide2.QtCore import QFile
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    ui_file = QFile("interface/interview_window.ui")
+    ui_file.open(QFile.ReadOnly)
+
+    loader = QUiLoader()
+    window = loader.load(ui_file)
+    ui_file.close()
+    window.show()
+
+    sys.exit(app.exec_())
