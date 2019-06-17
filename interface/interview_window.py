8a22e3e1e05c9327f465004729b0bc583dbebb91
Graidaris <vladislavby96@gmail.com>
2019-06-18 00:09:12 +0200

    update

--- interface/interview_window.py
+++ interface/interview_window.py
@@ -79,11 +79,11 @@ class Ui_InterviewWindow(object):
         self.pushButton_next = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_next.setObjectName("pushButton_next")
         self.horizontalLayout_buttons.addWidget(self.pushButton_next)
-        
+
         self.pushButton_finish = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_finish.setObjectName("pushButton_finish")
         self.horizontalLayout_buttons.addWidget(self.pushButton_finish)
-        
+
         self.verticalLayout_2.addLayout(self.horizontalLayout_buttons)
         InterviewWindow.setCentralWidget(self.centralwidget)
 
@@ -91,7 +91,6 @@ class Ui_InterviewWindow(object):
         QtCore.QMetaObject.connectSlotsByName(InterviewWindow)
 
         self.init_interview()
-
 
     def retranslateUi(self, InterviewWindow):
         InterviewWindow.setWindowTitle(QtWidgets.QApplication.translate(
@@ -115,16 +114,19 @@ class Ui_InterviewWindow(object):
         self.names_questions = []
         self.added_elements = {}
         self.added_widgets = []
-        self.verticalLayout_question_attr = QtWidgets.QVBoxLayout(self.options_widget_answer)
+        self.verticalLayout_question_attr = QtWidgets.QVBoxLayout(
+            self.options_widget_answer)
         self.nr_question = 0
         self.previous_nr_question = 0
         self.pushButton_finish.hide()
+
 
     def load_questions_json(self, path):
         with open(path, "r") as questions_json:
             self.questions = json.load(questions_json)
         self.add_question_attributes()
         self.update_question()
+
 
     def next_question(self):
         if self.nr_question + 1 < len(self.questions):
@@ -134,7 +136,7 @@ class Ui_InterviewWindow(object):
             if self.nr_question == len(self.questions) - 1:
                 self.pushButton_next.hide()
                 self.pushButton_finish.show()
-             
+
 
     def previous_question(self):
         if self.nr_question - 1 >= 0:
@@ -145,10 +147,12 @@ class Ui_InterviewWindow(object):
                 self.pushButton_next.show()
                 self.pushButton_finish.hide()
 
+
     def update_question(self):
         self.label_2.setText(str(self.nr_question + 1) +
-                                 ' of ' + str(len(self.questions)))
-        self.label.setText(self.questions[self.names_questions[self.nr_question]]["question"])
+                             ' of ' + str(len(self.questions)))
+        self.label.setText(
+            self.questions[self.names_questions[self.nr_question]]["question"])
 
         for widget in self.added_elements[self.names_questions[self.previous_nr_question]]:
             widget['widget'].hide()
@@ -161,29 +165,27 @@ class Ui_InterviewWindow(object):
         for name_question in self.questions:
             self.names_questions.append(name_question)
             self.added_elements[name_question] = []
-            
+
             for field_name in self.questions[name_question]["field_type"]:
                 if field_name == "int":
-                    self.add_int_field(
-                        self.questions[name_question]["field_type"][field_name], name_question)
+                    self.add_int_field(self.questions[name_question]["field_type"][field_name], name_question)
                 elif field_name == "radio":
-                    self.add_radio_field(
-                        self.questions[name_question]["field_type"][field_name], name_question)
+                    self.add_radio_field(self.questions[name_question]["field_type"][field_name], name_question)
                 elif field_name == "float":
-                    self.add_float_field(
-                        self.questions[name_question]["field_type"][field_name], name_question)
+                    self.add_float_field(self.questions[name_question]["field_type"][field_name], name_question)
 
             for widget in self.added_elements[name_question]:
                 widget['widget'].hide()
                 self.verticalLayout_question_attr.addWidget(widget['widget'])
 
 
-    def add_radio_field(self, attr, name_question):        
+    def add_radio_field(self, attr, name_question):
         widget_container = {
-                    'widget':QtWidgets.QWidget(self.options_widget_answer),
-                    'widget_list':[]
-                }
-        widget_container['layout'] = QtWidgets.QVBoxLayout(widget_container['widget'])
+            'widget': QtWidgets.QWidget(self.options_widget_answer),
+            'widget_list': []
+        }
+        widget_container['layout'] = QtWidgets.QVBoxLayout(
+            widget_container['widget'])
 
         for radio in attr:
             radio_button = {
@@ -191,14 +193,16 @@ class Ui_InterviewWindow(object):
                 'value': radio['value']
             }
             radio_button['widget'].setText(radio['text'])
-            
+
             if len(widget_container['widget_list']) < 1:
                 radio_button['widget'].setChecked(True)
-            
+
             widget_container['widget_list'].append(radio_button)
-            widget_container['layout'].addWidget(widget_container['widget_list'][-1]['widget'])
-            
+            widget_container['layout'].addWidget(
+                widget_container['widget_list'][-1]['widget'])
+
         self.added_elements[name_question].append(widget_container)
+
 
     def add_int_field(self, attr, name_question):
         spin_box = {
@@ -207,13 +211,15 @@ class Ui_InterviewWindow(object):
         spin_box['widget'].setMaximum(999)
         self.added_elements[name_question].append(spin_box)
 
+
     def add_float_field(self, attr, name_question):
         double_spin_box = {
             'widget': QtWidgets.QDoubleSpinBox(self.options_widget_answer)
         }
         double_spin_box['widget'].setMaximum(999.0)
         self.added_elements[name_question].append(double_spin_box)
-        
+
+
     def finish_interview(self):
         finish_data = []
         for name_question in self.names_questions:
@@ -223,7 +229,8 @@ class Ui_InterviewWindow(object):
                     for w_radio in widget['widget_list']:
                         if w_radio['widget'].isChecked():
                             result = w_radio['value']
-                            break                            
+                            break
+                    
                     finish_data.append(result)
                 else:
                     finish_data.append(widget['widget'].value())
